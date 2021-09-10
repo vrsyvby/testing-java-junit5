4d76032c484aa395011230e62d261f8e5db0c590
vrsyvby@gmail.com <3%@tatime>
2021-09-10 16:17:42 +0530

    circle ci build

--- src/test/java/guru/springframework/sfgpetclinic/CustomArgsProvider.java
+++ src/test/java/guru/springframework/sfgpetclinic/CustomArgsProvider.java
@@ -6,15 +6,13 @@ import org.junit.jupiter.params.provider.ArgumentsProvider;
 
 import java.util.stream.Stream;
 
-/**
- * Created by jt on 2018-10-28.
- */
 public class CustomArgsProvider implements ArgumentsProvider {
     @Override
     public Stream<? extends Arguments> provideArguments(ExtensionContext extensionContext) throws Exception {
-        return Stream.of(
-                Arguments.of("FL", 7, 10),
-                Arguments.of("OH", 11, 42),
-                Arguments.of("MI", 44, 77));
+       return Stream.of(
+               Arguments.of("FL",3,3),
+               Arguments.of("OH",5,6),
+               Arguments.of("TX",7,8)
+        );
     }
 }
